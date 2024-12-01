8aee69efc10a5bffc4121a1e1c5cd9689ad75d8b
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2024-11-30 23:35:18 -0600

    Add WebSocket server & client test

---
+++ tests/websockets/ws_client.js
@@ -0,0 +1,32 @@
+var W3CWebSocket = require('websocket').w3cwebsocket;
+let HOST = `localhost`;
+let PORT = 3000;
+let wsurl = `ws://${HOST}:${PORT}`;
+var client = new W3CWebSocket(wsurl, `echo-protocol`);
+
+client.onerror = function() {
+    console.log('Connection Error');
+};
+
+client.onopen = function() {
+    console.log('WebSocket Client Connected');
+
+    function sendNumber() {
+        if (client.readyState === client.OPEN) {
+            var number = Math.round(Math.random() * 0xFFFFFF);
+            client.send(number.toString());
+            setTimeout(sendNumber, 1000);
+        }
+    }
+    sendNumber();
+};
+
+client.onclose = function() {
+    console.log('echo-protocol Client Closed');
+};
+
+client.onmessage = function(e) {
+    if (typeof e.data === 'string') {
+        console.log("Received: '" + e.data + "'");
+    }
+};
