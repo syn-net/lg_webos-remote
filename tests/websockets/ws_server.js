2c66851b2d87164968d7c3f802ee3d2a5f6a486e
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2024-11-30 23:35:18 -0600

    Add WebSocket server & client test

---
+++ tests/websockets/ws_server.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+// https://stackoverflow.com/questions/61755518/is-it-possible-to-have-a-node-js-websocket-client
+
+// TODO(JEFF): This should be the w3cwebsocket variant
+// const WebSocketServer = require('websocket').w3cwebsocket;
+var WebSocketServer = require('websocket').server;
+var http = require('node:http');
+
+const PORT = 3000;
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(PORT, function() {
+    console.log((new Date()) + ` Server is listening on port ${PORT}`);
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
